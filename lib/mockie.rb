9e352870e4e67a2492af40c667dfa004d82f88df
Marius Bodnariuc <marius.bodnariuc@assist.ro>
2017-08-15 12:05:32 +0300

    Allow object to receive message and return value

--- lib/mockie.rb
+++ lib/mockie.rb
@@ -16,15 +16,21 @@ module Mockie
 
     def to(definition)
       @object.define_singleton_method(definition.message) do
+        definition.return_value
       end
     end
   end
 
   class StubDefinition
-    attr_reader :message
+    attr_reader :message, :return_value
 
     def initialize(message)
       @message = message
+    end
+
+    def and_return(value)
+      @return_value = value
+      self
     end
   end
 end
--- test/mockie_test.rb
+++ test/mockie_test.rb
@@ -11,4 +11,13 @@ class MockieTest < Test::Unit::TestCase
 
     assert_true(repo.respond_to?(:get_all))
   end
+
+  def test_mockie_allows_object_to_receive_message_and_return_value
+    repo = Object.new
+    people = [{id: 1, name: "John Doe"}, {id: 2, name: "Jane Doe"}]
+
+    allow(repo).to receive(:get_all).and_return(people)
+
+    assert_equal(people, repo.get_all)
+  end
 end
