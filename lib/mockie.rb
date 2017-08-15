13e6a17dec3aad7293c5dc7a424d981db9d00c6a
Marius Bodnariuc <marius.bodnariuc@assist.ro>
2017-08-15 12:00:58 +0300

    Allow object to receive message

--- lib/mockie.rb
+++ lib/mockie.rb
@@ -1,2 +1,30 @@
 module Mockie
+  module TestHelpers
+    def allow(object)
+      StubTarget.new(object)
+    end
+
+    def receive(message)
+      StubDefinition.new(message)
+    end
+  end
+
+  class StubTarget
+    def initialize(object)
+      @object = object
+    end
+
+    def to(definition)
+      @object.define_singleton_method(definition.message) do
+      end
+    end
+  end
+
+  class StubDefinition
+    attr_reader :message
+
+    def initialize(message)
+      @message = message
+    end
+  end
 end
--- test/mockie_test.rb
+++ test/mockie_test.rb
@@ -2,4 +2,13 @@ require 'test/unit'
 require_relative '../lib/mockie.rb'
 
 class MockieTest < Test::Unit::TestCase
+  include Mockie::TestHelpers
+
+  def test_mockie_allows_object_to_receive_message
+    repo = Object.new
+
+    allow(repo).to receive(:get_all)
+
+    assert_true(repo.respond_to?(:get_all))
+  end
 end
